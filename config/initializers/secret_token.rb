5650f07e97e07695559f7480429186b5c364c0dd
pacharanero <marcusbaw@gmail.com>
2013-07-21 10:51:39 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninmei::Application.config.secret_token = '65f3b3210fe3ba161520cf6e2d1cb753c4aa3463da4a053039531d204a11b844c65254126ce340df2a24860b72312d436986c5093769abde5b90baf5c2690bc1'
